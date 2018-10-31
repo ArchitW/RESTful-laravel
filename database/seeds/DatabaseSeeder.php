4589574dcf25623981f80628b195bfddd4cebc7b
ArchitW <archit2x3@hotmail.com>
2018-10-31 13:51:42 +0530

    seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
+use App\Category;
+use App\Product;
+use App\Transaction;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +16,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        //Empty all tables before seeding
+        DB::Statement('SET FOREIGN_KEY_CHECKS = 0'); //
+        User::truncate();
+        Category::truncate();
+        Product::truncate();
+        Transaction::truncate();
+        DB::table('category_product')->truncate(); // this table has no model
+
+        //Set of numbers of records to generate
+       $user_quantity = 200;
+       $category_quantity = 30;
+       $product_quantity = 1000;
+       $transaction_quantity = 1000;
+
+       factory(User::class, $user_quantity)->create();
+       factory(Category::class, $category_quantity)->create();
+       factory(Transaction::class, $transaction_quantity)->create();
+
+       factory(Product::class, $user_quantity)->create()->each(
+           // get each product, assign 5 categories to product
+         function($product){
+          $categories = Category::all()->random(mt_rand(1,5))->pluck('id');
+          $product->categories()->attach($categories);
+         });
     }
 }
