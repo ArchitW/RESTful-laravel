4d0e3d3394e41c48b8a895944ba95fb1c193b7f0
ArchitW <archit2x3@hotmail.com>
2018-10-31 13:37:25 +0530

    factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,8 +11,15 @@
 |
 */
 
+use App\User;
+use App\Category;
+use App\Product;
+use App\Transaction;
+use App\Seller;
+
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-$factory->define(App\User::class, function (Faker\Generator $faker) {
+/* Data For User */
+$factory->define(User::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
@@ -20,5 +27,47 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+        'verified' => $verified = $faker->randomElement([User::VERIFIED_USER, User::UNVERIFIED_USER]),
+        // if user is not verified generate verification token else null
+        'verification_token' => $verified == User::VERIFIED_USER ? null : User::genegateVerificationCode(),
+        'admin' => $faker->randomElement([User::ADMIN_USER, User::REGULAR_USER]),
+
     ];
 });
+
+/* Category Data */
+$factory->define(Category::class, function (Faker\Generator $faker) {
+
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+
+    ];
+});
+
+$factory->define(Product::class, function (Faker\Generator $faker) {
+
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+        'quantity' => $faker->randomNumber(1,10),
+        'status' => $faker->randomElement([Product::AVAILABLE_PRODUCT, Product::UNAVAILABLE_PRODUCT]),
+        'image' => $faker->randomElement(['1.jpg','2.jpg','3.jpg']), // images saved at public\img\
+        'seller_id' => User::all()->random(1)->id, //get all user -> get random 1 -> get his id
+       // Or User::inRandomOrder()->first()->id
+    ];
+});
+
+$factory->define(Transaction::class, function (Faker\Generator $faker) {
+
+    // seller must be different from buyer
+    $seller = Seller::has('products')->get()->random(); // get seller who have product
+    $buyer = User::all()->except($seller->id)->random(); // do not use this buyer as a seller
+    return [
+       'quantity' => $faker->numberBetween(1,3),
+        'buyer_id' => $buyer->id ,
+        'product_id' => $seller->products->random()->id, // get random product from this seller
+    ];
+});
+
+
