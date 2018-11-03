2fe092b59641c26dbb29d5f5b5610dc7d0716482
ArchitW <archit2x3@hotmail.com>
2018-11-03 11:36:16 +0530

    resolving buyer bindings using global scope

--- app/Buyer.php
+++ app/Buyer.php
@@ -2,10 +2,17 @@
 
 namespace App;
 
-
+use App\Scopes\BuyerScope;
 
 class Buyer extends User
 {
+ // 1st method gets executed during object creation
+    protected static function boot()
+{
+    parent::boot();
+    static::addGlobalScope(new BuyerScope());
+}
+
    public function transctions(){
        return $this->hasMany(Transaction::Class);
    }
